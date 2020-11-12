6498b9d36f1ac0947e26c59a0b5338781d652487
Are Sandvik <aresandvik@Ares-MacBook-Pro.local>
2020-11-12 20:29:37 +0100

    remove studio, rewrite App.js

---
+++ web/src/App.js
@@ -0,0 +1,60 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="name">Are W. Sandvik,<br/>Oslo, Norge</p>
+        <p>
+          Jeg er en innholdsdesigner og teknolog som jobber med kommunikasjon, designprosesser og webutvikling.
+        </p>
+        <p>
+          For tiden er fagleder for innhold i         <a
+          className="App-link"
+          href="https://www.netlife.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlife.
+        </a>
+        </p>
+        <p>Jeg skriver og mener om teknologi og samfunn, og samler det <a
+          className="App-link"
+          href="https://medium.com/@are.w.sandvik"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          på  Medium.
+        </a>
+        Ellers finner du meg på <a
+          className="App-link"
+          href="https://medium.com/@are.w.sandvik"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          på  Github, 
+        </a> 
+        tidvis 
+         <a
+          className="App-link"
+          href="https://twitter.com/AreSandvik"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        på Twitter, 
+        </a> 
+         og  <a
+          className="App-link"
+          href="https://www.netlife.com/folka/are-w-sandvik"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hos Netlife.
+        </a> 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
