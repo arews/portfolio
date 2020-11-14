7358afb981918d6000a996677fe28db99b6507ff
Are Sandvik <aresandvik@Ares-MacBook-Pro.local>
2020-11-14 11:06:33 +0100

    rewrite README and add favicon

--- web/src/App.js
+++ web/src/App.js
@@ -4,15 +4,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>
         <p className="name">Are W. Sandvik,<br/>Oslo, Norge</p>
         <p>
           Jeg er en innholdsdesigner og teknolog som jobber med kommunikasjon, designprosesser og webutvikling.
         </p>
         <p>
-          For tiden er fagleder for innhold i         <a
+          For tiden er jeg fagleder for innhold i         <a
           className="App-link"
           href="https://www.netlife.com"
-          target="_blank"
           rel="noopener noreferrer"
         >
           Netlife.
@@ -21,37 +21,23 @@ function App() {
         <p>Jeg skriver og mener om teknologi og samfunn, og samler det <a
           className="App-link"
           href="https://medium.com/@are.w.sandvik"
-          target="_blank"
           rel="noopener noreferrer"
         >
-          på  Medium.
-        </a>
-        Ellers finner du meg på <a
+          på  Medium.</a> Ellers finner du meg <a
           className="App-link"
           href="https://medium.com/@are.w.sandvik"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          på  Github, 
-        </a> 
-        tidvis 
-         <a
+          rel="noopener noreferrer"> på  Github,</a> tidvis <a
           className="App-link"
           href="https://twitter.com/AreSandvik"
-          target="_blank"
           rel="noopener noreferrer"
-        >
-        på Twitter, 
-        </a> 
-         og  <a
+        > på Twitter,</a> og <a
           className="App-link"
           href="https://www.netlife.com/folka/are-w-sandvik"
-          target="_blank"
           rel="noopener noreferrer"
-        >
-          hos Netlife.
+        > hos Netlife.
         </a> 
         </p>
+        </div>
       </header>
     </div>
   );
